f50bb498162516857a516476787e7ae1b0420c44
jim-cherian <jim.cherian82@yahoo.com>
2015-12-08 20:50:12 +0530

    Version 1.4

--- src/test/java/p1/C1.java
+++ src/test/java/p1/C1.java
@@ -14,7 +14,37 @@ public class C1 {
 		driver.close();
 		System.out.println("The test is completed");
 		System.out.println("There is a change in the code");
-		System.out.println("There is version 1.2 added to the code");
+		System.out.println("There is version 1.3 added to the code");
+		System.out.println("Test Case One with Thread Id:- "
+				+ Thread.currentThread().getId());
+	}
+	
+	@Test
+	public void maventestexample1()
+	{
+		WebDriver driver1 = new FirefoxDriver();
+		driver1.get("http://google.com");
+		driver1.manage().window().maximize();
+		driver1.close();
+		System.out.println("The test is completed");
+		System.out.println("There is a change in the code");
+		System.out.println("There is version 1.3 added to the code");
+		System.out.println("Test Case One with Thread Id:- "
+				+ Thread.currentThread().getId());
+	}
+	
+	@Test
+	public void maventestexample2()
+	{
+		WebDriver driver2 = new FirefoxDriver();
+		driver2.get("http://google.com");
+		driver2.manage().window().maximize();
+		driver2.close();
+		System.out.println("The test is completed");
+		System.out.println("There is a change in the code");
+		System.out.println("There is version 1.3 added to the code");
+		System.out.println("Test Case One with Thread Id:- "
+				+ Thread.currentThread().getId());
 	}
 
 }
